644ba8417fa5ff03955556240b4f36d3793b72c0
Rakesh Ravuri <rravuri@gmail.com>
2014-10-07 17:32:28 +0100

    Added http template zip support

--- test/typedef.js
+++ test/typedef.js
@@ -14,14 +14,14 @@ describe('typedef', function() {
 			return m.type == 'error';
 		}).firstOrDefault();
 		assert.equal(msg, null);
-		var typedef=res.model.Interfaces['A'].getTypedef('Identifier');
+		var typedef=res.model.Interfaces.A.getTypedef('Identifier');
 		assert.equal(typedef.Name, 'Identifier');
 		assert.equal(typedef.BaseTypes[0], 'string');
 	});
 	it('may be specified at service scope.', function() {
 		var idl = '@tidl 2.0.0; service S { \n' +
 			'typedef Id extends string{\n' +
-			'@description "test";' +
+			'@description "test \ndescription";' +
 			' }\n}';
 		var res = tidl.parse(idl);
 		var msg = from(res.messages).where(function(m) {
@@ -31,6 +31,6 @@ describe('typedef', function() {
 		var typedef=res.model.getTypedef('Id');
 		assert.equal(typedef.Name, 'Id');
 		assert.equal(typedef.BaseTypes[0], 'string');
-		//assert.equal(res.model.Interfaces['A'].getOperation('add').getDescription(), 'test');
+		assert.equal(typedef.getDescription(),'test \ndescription');
 	});
 });
